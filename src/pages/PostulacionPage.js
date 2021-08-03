a9310c7b8c58865fcc64233f1fc51787f784a674
Jordan Godoy <jordan.godoy@usach.cl>
2021-08-03 00:46:44 -0400

    Conexion backend con frontend vista ingresar datos del postulante
    Se realizo la conexion entre la vista y el backend, para obtener y guardar archivos

--- src/pages/PostulacionPage.js
+++ src/pages/PostulacionPage.js
@@ -5,78 +5,105 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-export default class Diplomados extends Component {
+
+export default class Postulacion extends Component {
+    
     constructor(props) {
-      super(props);
-      this.state = {
-        diplomados: [],
-        docentes : []
-      }
+        super(props);
+        this.state = {
+            diplomados: [], 
+            nombre: "",
+            correo: "",
+            telefono: ""
+        }
     }
-    //Created de de vue
+    
+    onSubmit = () => {
+        console.log(this.state.nombre);
+        const userObject = {
+            nombre: this.state.nombre,
+            correo: this.state.correo,
+            num_telefono: this.state.telefono
+        };
+
+        axios.post(process.env.REACT_APP_BASE_URL + 'postulantes/create', userObject)
+            .then((res) => {
+                console.log(res.data)
+            }).catch((error) => {
+                console.log(error)
+            });
+
+        this.setState({ nombre: '', correo: '', num_telefono: ''})
+    };
+
     componentDidMount() {
         //Obtener diplomados
-        axios.get(process.env.REACT_APP_BASE_URL + 'postulacion').then(res => {
+        axios.get(process.env.REACT_APP_BASE_URL + 'diplomados').then(res => {
             var diplomados = res.data;
             this.setState({ diplomados });
-            this.getDocentes();
-          }).catch(error => {
+            this.getAllDiplomado();
+        }).catch(error => {
             console.log(error)
-         });
+        });
     }
+
     render() {
         return (
-            <div>
-              
+            <div>      
               <Container className="justify-content-md-center">
-                  <br></br>
-                  <h1 > Ingresa tu Postulación! </h1>
-                  <br></br>
-                <Row >  
-                <Form>
-
-                <Form.Label>Diplomado</Form.Label>    
-                <Form.Select aria-label="Default select example">
-                    <option>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </Form.Select>
-                
-                    <Form.Group className="mb-3" controlId="formBasicName">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="name" placeholder="Enter Name" />
-                        <Form.Text className="text-muted">
-                        We'll never share your name with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
-                        <Form.Label>Phone Number</Form.Label>
-                        <Form.Control type="num_telefono" placeholder="Phone Number" />
-                    </Form.Group>
-
-                    <Form.Group controlId="formFileMultiple" className="mb-3">
-                    <Form.Label>Multiple files input example</Form.Label>
-                    <Form.Control type="file" multiple />
-                    </Form.Group>
-
-                    <Col md={{ span: 6, offset: 6 }}>
-                    <Button  variant="primary" type="submit">
-                        Submit
-                    </Button>
+                    <br></br>
+                    <Col md={{ span: 6, offset: 4 }}>
+                        <h1 > Ingresa tu Postulación! </h1>
                     </Col>
-                </Form></Row>
+
+                    <Row >  
+                        <Form>
+                            <Form.Label>Diplomado</Form.Label>    
+                            <Form.Select aria-label="Default select example">
+                                <option>Seleccione el diplomado al que desea postular</option>
+                                {this.state.diplomados.map(diplomado => <option value="1">{diplomado.titulo}</option>)}
+                            </Form.Select>
+                            <br></br>
+                            <Form.Group className="mb-3" controlId="formBasicName">
+                                <Form.Label>Nombre</Form.Label>
+                                <Form.Control type="name" placeholder="Ingrese su nombre" value={this.state.nombre} onChange={e => this.setState({ nombre: e.target.value })}/>
+                                <Form.Text className="text-muted">
+                                Ingrese su nombre completo.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Correo</Form.Label>
+                                <Form.Control type="email" placeholder="Ingrese su correo" value={this.state.correo} onChange={e => this.setState({ correo: e.target.value })}/>
+                                <Form.Text className="text-muted">
+                                Ingrese un correo de contacto.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
+                                <Form.Label>Número teléfonico</Form.Label>
+                                <Form.Control type="num_telefono" placeholder="Ingrese su número de teléfono" value={this.state.telefono} onChange={e => this.setState({ telefono: e.target.value })}/>
+                                <Form.Text className="text-muted">
+                                El número debe contener 9 digitos.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group controlId="formFileMultiple" className="mb-3">
+                            <Form.Label>Escoja los archivos que desea subir</Form.Label>
+                            <Form.Control type="file" multiple />
+                            </Form.Group>
+                            <br></br>
+
+                            <Col md={{ span: 5, offset: 6 }}>
+                                <Button variant="primary" type="submit" onClick={this.onSubmit}>
+                                    Enviar
+                                </Button>
+                                
+                            </Col>
+
+                        </Form>
+                    </Row>
                 </Container>
-              
             </div>
         )
     }
